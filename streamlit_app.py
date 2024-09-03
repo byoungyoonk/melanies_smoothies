9e4b25ea1a24a7ed1e9ae855c99426d97bff6778
tokyobang <97203912+byoungyoonk@users.noreply.github.com>
2024-09-03 19:36:16 +0900

    Update streamlit_app.py
    import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
